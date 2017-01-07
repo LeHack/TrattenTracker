d0c36cc06d1b78fd22940287c183cb36a63cda32
skurski <peterskurski@gmail.com>
2017-01-07 19:51:04 +0100

    Group setup view #5

--- ttapp/admin.py
+++ ttapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from ttapp.models import Groups, TrainingSchedule
 
-# Register your models here.
+class TrainingScheduleInline(admin.TabularInline):
+    model = TrainingSchedule
+    extra = 0
+    ordering = ('dow', )
+
+class GroupsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'monthly_fee')
+    list_filter = ['name', 'monthly_fee']
+    search_fields = ['name', 'monthly_fee']
+    inlines = [TrainingScheduleInline]
+
+admin.site.register(Groups, GroupsAdmin)
