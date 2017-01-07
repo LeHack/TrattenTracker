174f6b90386d78b8a761bd19d106edb3760e7915
skurski <peterskurski@gmail.com>
2017-01-07 22:13:22 +0100

    Attendee managment view #6

--- ttapp/admin.py
+++ ttapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from ttapp.models import Groups, TrainingSchedule
+from ttapp.models import Groups, TrainingSchedule, Attendees
 
 class TrainingScheduleInline(admin.TabularInline):
     model = TrainingSchedule
@@ -10,6 +10,14 @@ class GroupsAdmin(admin.ModelAdmin):
     list_display = ('name', 'monthly_fee')
     list_filter = ['name', 'monthly_fee']
     search_fields = ['name', 'monthly_fee']
+    ordering = ('monthly_fee', )
     inlines = [TrainingScheduleInline]
 
+class AttendeesAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'assigned_pin', 'has_sport_card')
+    list_filter = ['group', 'has_sport_card']
+    search_fields = ['first_name', 'last_name']
+    ordering = ('last_name', 'first_name')
+
 admin.site.register(Groups, GroupsAdmin)
+admin.site.register(Attendees, AttendeesAdmin)
