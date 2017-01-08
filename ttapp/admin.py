4594de8b25db14b41b0a9bf1242a4c69b2e2d8b1
skurski <peterskurski@gmail.com>
2017-01-08 14:13:54 +0100

    Cancelled training management #9

--- ttapp/admin.py
+++ ttapp/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
-from django import forms
-from ttapp.models import Groups, TrainingSchedule, Attendees, TrainingMonth
-from django.db import models
+from ttapp.models import Groups, TrainingSchedule, Attendees, CancelledTrainings
 
-import datetime
 
 class TrainingScheduleInline(admin.TabularInline):
     model = TrainingSchedule
@@ -23,40 +20,12 @@ class AttendeesAdmin(admin.ModelAdmin):
     search_fields = ['first_name', 'last_name']
     ordering = ('last_name', 'first_name')
 
-# class TrainingMonthModelForm(forms.ModelForm):
-#
-#     class Meta:
-#         model = TrainingMonth
-#         fields = ('group', 'year', 'month', 'day_count')
-#
-#     def __init__(self, *args, **kwargs):
-#         super(TrainingMonth, self).__init__(*args, **kwargs)
-#         self.fields['day_count'].initial = 999
-
-
-class TrainingMonthAdmin(admin.ModelAdmin):
-    model = TrainingMonth
-    fields = ('group', 'year', 'month', 'day_count')
-    list_display = ['__str__', 'group', 'day_count', ]
-
-    def save_model(self, request, obj, form, change):
-        if obj.day_count == 0:
-            obj.day_count = self.calc_day_count(obj)
-        obj.save()
-
-    def calc_day_count(self, obj):
-        trainingSchedules = TrainingSchedule.objects.filter(group=obj.group).values('dow').values_list('dow', flat=True)
-        holidays = [datetime.date(2017, 12, 25)] # you can add more here
-        training_count = 0
-        for i in range(1, 32):
-            try:
-                thisdate = datetime.date(obj.year, obj.month, i)
-            except(ValueError):
-                break
-            if thisdate.weekday() in trainingSchedules and thisdate not in holidays:  # Monday == 0, Sunday == 6
-                training_count += 1
-        return training_count
+class CancelledTrainingsAdmin(admin.ModelAdmin):
+    model = CancelledTrainings
+    list_display = ('schedule', 'date')
+    list_filter = ['date']
+    ordering = ('date',)
 
 admin.site.register(Groups, GroupsAdmin)
 admin.site.register(Attendees, AttendeesAdmin)
-admin.site.register(TrainingMonth, TrainingMonthAdmin)
+admin.site.register(CancelledTrainings, CancelledTrainingsAdmin)
